bcec5d684b842fae57e0bff8b485ed55dbd588ae
Iago lira <iagolirapassos@gmail.com>
2019-12-15 19:49:25 -0300

    Testes com PID

--- SeguidorDeLinhasIDB.ino
+++ SeguidorDeLinhasIDB.ino
@@ -1,72 +1,28 @@
-/*
- * kP -- const. proporcional
- * kI -- const. Integral
- * kD -- const. derivada
- * mediaS -- media dos sensores de referencial
- */
-int IRO()
-{
-  int O1 = analogRead(IRO1);
-  int O2 = analogRead(IRO2);
-  int O3 = analogRead(IRO3);
+#include "control.h"
 
-  return (O1 + O2 + O3);
-}
-
-int IRLinha()
+void setup()
 {
- int x1 = analogRead(IRL1);
- int x2 = analogRead(IRL2);
- 
- return (x1 + x2);  
-}
-
-void motorPWM(int x1, int x2)
-{
-  Serial.print("Olá");
-  if (x1 > 0) {
-    analogWrite(MotorEsquerdoFrente, x1);
-    analogWrite(MotorEsquerdoTras, 0);
-  }
-  else if (x1 == 0) {
-    analogWrite(MotorEsquerdoFrente, 0);
-    analogWrite(MotorEsquerdoTras, 0);
-  }
-  else {
-    analogWrite(MotorEsquerdoFrente, 0);
-    analogWrite(MotorEsquerdoTras, abs(x1));
-  }
+  Serial.begin(9600);
   
-  if (x2 > 0) {
-    analogWrite(MotorDireitoFrente, x2);
-    analogWrite(MotorDireitoTras, 0);
-  }
-  else if (x2 == 0) {
-    analogWrite(MotorDireitoFrente, 0);
-    analogWrite(MotorDireitoTras, 0);
-  }
-  else {
-    analogWrite(MotorDireitoFrente, 0);
-    analogWrite(MotorDireitoTras, abs(x2));
-  }
+  pinMode(M1D, OUTPUT);
+  pinMode(M1U, OUTPUT);
+  pinMode(M2D, OUTPUT);
+  pinMode(M2U, OUTPUT);
+  
+  //Portas IRO
+//  pinMode(IRO1, INPUT);
+//  pinMode(IRO2, INPUT);
+//  pinMode(IRO3, INPUT);
+  
+  //Portas IRLinhas
+  //pinMode(IRL1, OUTPUT);
+  //pinMode(IRL2, OUTPUT);
 }
 
-void PID(double kp, double kI,, double kI, double PWM, double mediaS)
-}
-  double erro = lerSensores - mediaS;
-  double lastErro, PID, P, I, D;
-  int motor1, motor2;
-
-  P = erro*kP;
-  I = I + erro*kI;
-  D = erro*(erro - lastErro);
-
-  PID = P + I + D;
-
-  lastErro = erro;
-
-  motor1 = PWM + PID;
-  motor2 = PWM - PID;
-
-  motorPWM(motor1, motor2);
+void loop()
+{
+  //PID(double kp, double kI,, double kI, double PWM, double mediaS)
+   PID(0.39, 0.0, 0.0, 50, 44);
+  //PWM(60, 60);
+  //Serial.println((int)((analogRead(IRL1)+analogRead(IRL2))/2));
 }
--- control.h
+++ control.h
@@ -3,69 +3,95 @@
  * kI -- const. Integral
  * kD -- const. derivada
  * mediaS -- media dos sensores de referencial
+ * 
  */
-int IRO()
-{
-  int O1 = analogRead(IRO1);
-  int O2 = analogRead(IRO2);
-  int O3 = analogRead(IRO3);
+//Portas IRLinhas
+#define IRL1 A0 //Direita
+#define IRL2 A1 //Esquerda
 
-  return (O1 + O2 + O3);
-}
+//Portas ponte H
+#define M1D 11
+#define M1U 10
+#define M2D 6
+#define M2U 5
 
-int IRLinha()
+//Portas IR obstáculo
+//#define IRO1
+//#define IRO2
+//#define IRO3
+
+//Constantes
+int mediaS = 100;
+
+//void lerSensoresObstaculo()
+//{
+//  int O1 = analogRead(IRO1);
+//  int O2 = analogRead(IRO2);
+//  int O3 = analogRead(IRO3);
+//}
+
+int lerSensorLinha()
 {
  int x1 = analogRead(IRL1);
  int x2 = analogRead(IRL2);
  
- return (x1 + x2);  
+ return (x1 + x2)/2;  
 }
 
-void motorPWM(int x1, int x2)
+
+void PWM(int x1, int x2)
 {
+   // MOTOR ESQUERDO
+  // motor esquerdo para frente
   if (x1 > 0) {
-    analogWrite(MotorEsquerdoFrente, x1);
-    analogWrite(MotorEsquerdoTras, 0);
+    analogWrite(M2U, x1);
+    analogWrite(M2D, 0);
+    
   }
+  // motor esquerdo "travado"
   else if (x1 == 0) {
-    analogWrite(MotorEsquerdoFrente, 0);
-    analogWrite(MotorEsquerdoTras, 0);
+    analogWrite(M2U, 0);
+    analogWrite(M2D, 0);
   }
+  // Motor esquerdo para tras
   else {
-    analogWrite(MotorEsquerdoFrente, 0);
-    analogWrite(MotorEsquerdoTras, abs(x1));
+    analogWrite(M2U, 0);
+    analogWrite(M2D, abs(x1));
   }
-  
+
+
+  // MOTOR DIREITO
   if (x2 > 0) {
-    analogWrite(MotorDireitoFrente, x2);
-    analogWrite(MotorDireitoTras, 0);
+    analogWrite(M1U, x2);
+    analogWrite(M1D, 0);
   }
   else if (x2 == 0) {
-    analogWrite(MotorDireitoFrente, 0);
-    analogWrite(MotorDireitoTras, 0);
+    analogWrite(M1U, 0);
+    analogWrite(M1D, 0);
   }
   else {
-    analogWrite(MotorDireitoFrente, 0);
-    analogWrite(MotorDireitoTras, abs(x2));
+    analogWrite(M1U, 0);
+    analogWrite(M1D, abs(x2));
   }
 }
 
-void PID(double kp, double kI,, double kI, double PWM, double mediaS)
-}
-  double erro = lerSensores - mediaS;
-  double lastErro, PID, P, I, D;
+
+void PID(float kP, float kI, float kD, int PWM2, int mediaS)
+{
+  float erro = (float)lerSensorLinha() - (float)mediaS;
+  float lastErro, PID2, P, I, D;
   int motor1, motor2;
 
   P = erro*kP;
-  I = I + erro*kI;
-  D = erro*(erro - lastErro);
+  I = I + (erro*kI);
+  D = kD*(erro - lastErro);
 
-  PID = P + I + D;
+  PID2 = P + I + D;
 
   lastErro = erro;
 
-  motor1 = PWM + PID;
-  motor2 = PWM - PID;
+  motor1 = PWM2 + (int)PID2;
+  motor2 = PWM2 - (int)PID2;
 
-  motorPWM(motor1, motor2);
+  PWM(motor1, motor2);
 }
